922ae566e3bb0b572bd8f44f2421f37615104acd
Cupang5 <khanalfarizzi@hotmail.com>
2022-05-21 12:53:19 +0700

    Initial Commit

---
+++ Shared/WeSplit_Paul_Hudosn_App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_Paul_Hudosn_App.swift
+//  Shared
+//
+//  Created by Luthfi Khan on 21/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_Paul_Hudosn_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
